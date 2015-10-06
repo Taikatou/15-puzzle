b6c44a35dace7c37caf4d03d834c630451c4489f
Taikatou <13123947@studentmail.ul.ie>
2015-10-06 09:37:15 +0100

    15 puzzle
    15 puzzle based off Xuzzle

---
+++ Xuzzle/Xuzzle/menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xuzzle
+{
+    public class menu : ContentPage
+    {
+        Button fifteenButton;
+        Button eightButton;
+        public menu()
+        {
+            eightButton = new Button
+            {
+                Text = "8 puzzle",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            eightButton.Clicked += eightClick;
+
+            fifteenButton = new Button
+            {
+                Text = "15 puzzle",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            fifteenButton.Clicked += fifteenClick;
+
+            Content = new StackLayout
+            {
+                Children = {
+                    eightButton,
+                    fifteenButton,
+                }
+            };
+        }
+
+        async void eightClick(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new XuzzlePage(3));
+        }
+
+        async void fifteenClick(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new XuzzlePage(4));
+        }
+    }
+}
